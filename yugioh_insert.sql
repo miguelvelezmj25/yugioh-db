cb574f35aceea1c7ae239903780183be9f794fd9
Miguel Velez <miguelvelez@mijecu25.com>
2015-11-21 08:42:38 -0600

    Added insert and update commands

---
+++ yugioh_insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO cards (cards.pack, cards.id, cards.name, cards.edition, cards.rarity, cards.`type`) VALUES ("LCYW", "EN096", "Magical Hats", "1E", "C", "Trap");
+
+-- INSERT INTO edition (edition.edition, edition.edition_name) VALUES ("N", "Normal");
+
+-- INSERT INTO packs (packs.pack, packs.pack_name) VALUES ("SDK", "Starter Deck Kaiba");
+
+-- INSERT INTO rarities (rarities.rarity, rarities.rarity_name, rarities.Description) VALUES ("SE", "Secret Rare","Holographic silver card name and parallel holofoil image");
---
+++ yugioh_update.sql
@@ -0,0 +1,1 @@
+UPDATE cards SET cards.`type` = 'Monster' WHERE type = 'Normal';
